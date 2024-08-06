870916d7ab357671183f5ffc9a6f4efa8070ac7f
JayGhz <jaykansas7@gmail.com>
2024-08-05 23:55:11 -0500

    chore: Add .gitignore and select.sql files

---
+++ Statements/select.sql
@@ -0,0 +1,8 @@
+-- Selecciona todos los registros de la tabla 'users'
+SELECT * FROM users;
+
+-- Selecciona el campo 'name' de todos los registros de la tabla 'users'
+SELECT name FROM users;
+
+-- Selecciona los campos 'name' y 'email' de todos los registros de la tabla 'users'
+SELECT name, email FROM users;
