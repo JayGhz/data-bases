dba63b6ae0139f7c2d8776d4f564967a43f09267
JayGhz <jaykansas7@gmail.com>
2024-08-10 01:36:31 -0500

    Refactor SQL statements for selecting data from 'users' table and add new query for selecting users with name 'ArianaMS' ordered by age

--- Statements/04-orderby.sql
+++ Statements/04-orderby.sql
@@ -3,3 +3,6 @@ SELECT * FROM users ORDER BY age;
 
 -- Seleccionar todos los usuarios ordenados por su edad de forma descendente
 SELECT * FROM users ORDER BY age DESC;
+
+-- Seleccionar todos los usuarios con nombre 'ArianaMs' ordenados por su edad
+SELECT * FROM users WHERE name='ArianaMS' ORDER BY age DESC;
